3bf56fbaebad8c332ef561a4af56be1734bc63f1
Quyet <huuquyetng@gmail.com>
2024-05-11 03:32:40 +0700

    feat: Init simple Huggingface api inference

---
+++ generate.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+from os import getenv
+import requests
+
+# Load environment variables from the .env file (if present)
+load_dotenv()
+
+API_TOKEN = getenv("HF_API_TOKEN")
+API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    if response.status_code == 200:
+        with open("wallpaper.png", "wb") as f:
+            f.write(response.content)
+
+data = query({        
+  "inputs": "A luminous field of fireflies blinking rhythmically against the deep velvet of a summer night",
+})
